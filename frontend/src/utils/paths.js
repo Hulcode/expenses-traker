10b742c375f0fd6f315b2bde3fe6d9dd5c6dbb13
hulkode <alhassanahmedsoliman@gmail.com>
2026-06-14 00:49:37 +0300

    tracker

--- frontend/src/utils/paths.js
+++ frontend/src/utils/paths.js
@@ -1,5 +1,4 @@
-export const BASE_URL = import.meta.env.BASE_URL || "http://localhost:5000";
-
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 // utils/apiPaths.j
 export const API_PATHS = {
   AUTH: {
