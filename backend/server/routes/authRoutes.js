475406a4483a125b742376412a2f0ca08eba2333
hulkode <alhassanahmedsoliman@gmail.com>
2026-06-14 11:25:48 +0300

    tracker

--- backend/server/routes/authRoutes.js
+++ backend/server/routes/authRoutes.js
@@ -15,7 +15,7 @@ router.post("/login", loginUser);
 
 router.get("/getUser", jwtCheck, getUserInfo);
 
-router.post("/upload-image", jwtCheck, upload.single("image"), (req, res) => {
+router.post("/upload-image", upload.single("image"), (req, res) => {
   if (!req.file) return res.status(400).json({ message: "No file uploaded" });
   res.status(200).json({ imageUrl: req.file.path });
 });
